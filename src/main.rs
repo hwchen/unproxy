869b24f34cb262c8f0e1bbf873b622048b1f7206
Walther Chen <walther.chen@gmail.com>
2019-10-02 22:50:31 -0400

    basic proxy fn

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use snafu::{Snafu, ResultExt, OptionExt};
 use std::net::SocketAddr;
 use std::time::{Duration, Instant};
 use structopt::StructOpt;
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 use tokio_io::{AsyncReadExt, AsyncWriteExt};
 
 #[tokio::main]
@@ -16,21 +16,27 @@ async fn main() -> Result<(), Error> {
     let to_address: SocketAddr = opt.to_address.parse()
         .context(InvalidAddress)?;
 
-    let mut listener = TcpListener::bind(&from_address)
+    let mut from_listener = TcpListener::bind(&from_address)
         .await
         .expect("could not bind to tcp socket");
 
     loop {
-        let (mut socket, _) = listener.accept()
+        let (mut from_socket, _) = from_listener.accept()
             .await
             .context(TcpSocket)?;
 
         // for each socket, echo for now
         tokio::spawn(async move {
+            let mut to_socket = TcpStream::connect(to_address)
+                .await
+                .expect("Failed to connect to target");
+            let (mut to_socket_read, mut to_socket_write) = to_socket.split();
+
             let mut buf = [0u8; 1024];
 
             loop {
-                let n = match socket.read(&mut buf).await {
+                // first half, go from -> to
+                let n = match from_socket.read(&mut buf).await {
                     Ok(n) if n == 0 => return,
                     Ok(n) => n,
                     Err(err) => {
@@ -39,7 +45,22 @@ async fn main() -> Result<(), Error> {
                     },
                 };
 
-                if let Err(err) = socket.write_all(&buf[0..n]).await {
+                if let Err(err) = to_socket_write.write_all(&buf[0..n]).await {
+                    eprintln!("Failed to write to socket: {}", err);
+                    return;
+                }
+
+                // second half, go to -> from
+                let n = match to_socket_read.read(&mut buf).await {
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(err) => {
+                        eprintln!("Failed to read from socket: {}", err);
+                        return;
+                    },
+                };
+
+                if let Err(err) = from_socket.write_all(&buf[0..n]).await {
                     eprintln!("Failed to write to socket: {}", err);
                     return;
                 }
