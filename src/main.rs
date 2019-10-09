765a964769ef32aae27e256884e08ea371aaa220
Walther Chen <walther.chen@gmail.com>
2019-10-09 15:21:54 -0400

    Explicitly shut down tcp write half
    For now, can't use tokio::io::copy because I can't access the halves
    in the result after the copy.
    
    The logic I'm using for copying might be a little less efficient than
    tokio::io::copy. However, for connection testing, it will probably be
    more flexible than just using tokio::io::copy anyways.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use snafu::{Snafu, ResultExt};
 use std::net::SocketAddr;
 use structopt::StructOpt;
 use tokio::net::{TcpListener, TcpStream};
-use tokio::io::AsyncReadExt;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -36,16 +36,12 @@ async fn main() -> Result<(), Error> {
             let (mut to_socket_read, mut to_socket_write) = to_socket.split();
             let (mut from_socket_read, mut from_socket_write) = from_socket.split();
 
-            // TODO shutdown of to_socket never happens because it never receives a shutdown
-            // signal. Do I need to write a new wrapper to handle that? I don't know that
-            // this basic futures mechanism can do it, since it has to happen after
-            // the to_socket is finished responding.
             try_join(
-                from_socket_read.copy(&mut to_socket_write)
+                copy(&mut from_socket_read, &mut to_socket_write)
                     .map_ok(|b| {
                         println!("from socket closed, {} bytes transferred", b);
                     }),
-                to_socket_read.copy(&mut from_socket_write)
+                copy(&mut to_socket_read, &mut from_socket_write)
                     .map_ok(|b| {
                         println!("to socket closed, {} bytes transferred", b);
                     }),
@@ -55,6 +51,42 @@ async fn main() -> Result<(), Error> {
             .expect("Unable to read or write to sockets");
         });
     }
+}
+
+/// Does normal copying with the addition that the write half of the
+/// TcpStream needs to be shutdown explicitly.
+/// Would normally use tokio::io::copy, but it doesn't call shutdown,
+/// and it doesn't pass through the read/write halves in the result, only
+/// the bytes transferred.
+async fn copy<R, W>(read_socket: &mut R, write_socket: &mut W) -> Result<u64, Error>
+    where R: AsyncReadExt + std::marker::Unpin,
+          W: AsyncWriteExt + std::marker::Unpin,
+{
+    let mut buf = [0; 1024];
+    let mut bytes_read = 0;
+
+    loop {
+        // read from listening side
+        let n = match read_socket.read(&mut buf).await {
+            Ok(n) if n == 0 => break,
+            Ok(n) => n,
+            Err(err) => {
+                return Err(err).context(Io)?;
+            }
+        };
+
+        // copy to target sid
+        write_socket.write_all(&buf[0..n]).await
+            .context(Io)?;
+
+        bytes_read += n as u64;
+    }
+
+    // Now that the copy is done, send the shutdown signal explicitly
+    write_socket.shutdown().await
+        .context(Io)?;
+
+    Ok(bytes_read)
 }
 
 #[derive(Debug, StructOpt)]
@@ -77,4 +109,6 @@ pub enum Error {
     InvalidAddress { source: std::net::AddrParseError },
     #[snafu(display("Tcp Socket Error: {}", source))]
     TcpSocket { source: tokio::io::Error },
+    #[snafu(display("IO Error: {}", source))]
+    Io { source: std::io::Error },
 }
