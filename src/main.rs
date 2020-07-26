5fa9152fe0d6c35180a1a9d5bae07f889d5e42fb
Walther Chen <walther.chen@gmail.com>
2020-07-26 00:03:18 -0400

    switch from tokio to async-executor

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,61 @@
-use anyhow::Error;
+use anyhow::{Context as _, Result};
+use async_dup::Arc;
+use async_executor::{LocalExecutor, Task};
+use async_io::Async;
+use futures_lite::io::{AsyncReadExt, AsyncWriteExt};
 use futures_util::future::{try_join, TryFutureExt};
-use std::net::SocketAddr;
+use std::net::{Shutdown, SocketAddr, TcpListener, TcpStream};
 use structopt::StructOpt;
-use tokio::net::{TcpListener, TcpStream};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
+fn main() -> Result<()> {
     let opt = CliOpt::from_args();
 
     let from_address: SocketAddr = opt.from_address.parse()?;
     let to_address: SocketAddr = opt.to_address.parse()?;
 
+    // set up executor
+    let local_ex = LocalExecutor::new();
+
+    local_ex.run(serve(from_address, to_address))?;
+
+    Ok(())
+}
+
+async fn serve(from_address: SocketAddr, to_address: SocketAddr) -> Result<()> {
     // bind listener now; target service is connected when
     // new connection is received on listener.
-    let mut from_listener = TcpListener::bind(&from_address)
-        .await
-        .expect("could not bind to tcp socket");
+    let from_listener = Async::<TcpListener>::bind(from_address)
+        .with_context(|| format!("could not bind to tcp socket: {}", from_address))?;
 
     // loop over new connections
     loop {
-        let (mut from_socket, _) = from_listener.accept()
+        let (from_socket, _) = from_listener.accept()
             .await?;
 
         // when new connection received, spawn a new task
-        tokio::spawn(async move {
+        Task::local(async move {
             // in this new task, connect to target service
-            let mut to_socket = match TcpStream::connect(to_address).await {
+            let to_socket = match Async::<TcpStream>::connect(to_address).await {
                 Ok(s) => s,
                 Err(err) => {
                     println!("Could not connect to target socket: {}", err);
                     return;
                 },
             };
-            let (mut to_socket_read, mut to_socket_write) = to_socket.split();
-            let (mut from_socket_read, mut from_socket_write) = from_socket.split();
+            let to_socket_read = Arc::new(to_socket);
+            let to_socket_write = to_socket_read.clone();
+            let from_socket_read = Arc::new(from_socket);
+            let from_socket_write = from_socket_read.clone();
 
             let _ = try_join(
-                copy(&mut from_socket_read, &mut to_socket_write),
-                copy(&mut to_socket_read, &mut from_socket_write),
+                copy(from_socket_read, to_socket_write),
+                copy(to_socket_read, from_socket_write),
             )
             .map_ok(|(b_tx, b_rx)| println!("-> {} bytes\n<- {} bytes", b_tx, b_rx))
             .map_err(|err| println!("Unable to read or write to sockets: {}", err))
             .await;
-        });
+        })
+        .detach();
     }
 }
 
@@ -52,9 +64,7 @@ async fn main() -> Result<(), Error> {
 /// Would normally use tokio::io::copy, but it doesn't call shutdown,
 /// and it doesn't pass through the read/write halves in the result, only
 /// the bytes transferred.
-async fn copy<R, W>(read_socket: &mut R, write_socket: &mut W) -> Result<u64, Error>
-    where R: AsyncReadExt + std::marker::Unpin,
-          W: AsyncWriteExt + std::marker::Unpin,
+async fn copy(mut read_socket: Arc<Async<TcpStream>>, mut write_socket: Arc<Async<TcpStream>>) -> Result<u64>
 {
     let mut buf = [0; 1024];
     let mut bytes_read = 0;
@@ -76,7 +86,7 @@ async fn copy<R, W>(read_socket: &mut R, write_socket: &mut W) -> Result<u64, Er
     }
 
     // Now that the copy is done, send the shutdown signal explicitly
-    write_socket.shutdown().await?;
+    write_socket.write_with(|w| w.shutdown(Shutdown::Write)).await?;
 
     Ok(bytes_read)
 }
